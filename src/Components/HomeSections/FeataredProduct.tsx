ca3ef3f49bfca0fbbd6ead46f0815671bb36e7f0
darkghost87-lab <www.darkghost87@gmail.com>
2026-08-30 18:07:17 +0330

    Done Productdetail page✅

--- src/Components/HomeSections/FeataredProduct.tsx
+++ src/Components/HomeSections/FeataredProduct.tsx
@@ -1,11 +1,6 @@
-import { Link } from "react-router-dom";
-import useFetch from "../../Hooks/useFetch";
-import type { Products } from "../../Types/Product";
+import ProductCard from "../ProductCard";
 
 const Feataredproduct = () => {
-  const { data: dataFetch, error } = useFetch<Products[]>({
-    url: "http://localhost:8000/Product",
-  });
   return (
     <>
       <section className="w-[100%] h-[60vh] bg-[#1F1F27] border-t border-t-[#8B5CF6] m-auto mt-20 px-5">
@@ -15,26 +10,7 @@ const Feataredproduct = () => {
         >
           <h2 className="underline p-[10px]">Featared Product</h2>
           <div className="size-full flex justify-center items-center gap-10">
-            {dataFetch?.map((item) => (
-              <Link to={`/productdetail/${item.id}`}>
-                <article
-                  className="w-[250px] h-[300px] flex flex-col justify-center items-center gap-2 bg-[#1F1F27] border border-[#8B5CF6] rounded duration-300 hover:translate-y-[-10px]"
-                  key={item.id}
-                >
-                  <img
-                    src={item.image}
-                    alt={item.name}
-                    className="w-[150px] h-[150px] object-cover"
-                  />
-                  <h2>{item.name}</h2>
-                  <p>{item.price}</p>
-                  <button className="w-[160px] h-[2rem] rounded duration-600 bg-[#7b55f7] hover:bg-[#A855F7] hover:text-zinc-950 text-zinc-100 mx-[15px] px-[10px] cursor-pointer">
-                    Add to Cart
-                  </button>
-                </article>
-              </Link>
-            ))}
-            {error && <p>{error.message}</p>}
+            <ProductCard />
           </div>
         </div>
       </section>
