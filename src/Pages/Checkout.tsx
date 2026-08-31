faf35622ea9768dc3508f087edc4ba0c527de020
darkghost87-lab <www.darkghost87@gmail.com>
2026-09-01 01:40:29 +0330

    Start checkout page🔥🫡

--- src/Pages/Checkout.tsx
+++ src/Pages/Checkout.tsx
@@ -8,23 +8,23 @@ const Checkout = () => {
             <div className="w-full h-[70%] flex flex-col items-center gap-8">
               <div className="w-[50%] flex flex-col justify-center items-start">
                 <label htmlFor="">Name</label>
-                <input type="text" className="w-full border rounded p-1 px-2" />
+                <input type="text" className="w-full border border-[#8B5CF6] rounded p-1 px-2 " />
               </div>
               <div className="w-[50%] flex flex-col justify-center items-start">
                 <label htmlFor="">last name</label>
-                <input type="text" className="w-full border rounded p-1 px-2" />
+                <input type="text" className="w-full border border-[#8B5CF6] rounded p-1 px-2 " />
               </div>
               <div className="w-[50%] flex flex-col justify-center items-start">
                 <label htmlFor="">address</label>
-                <input type="text" className="w-full border rounded p-1 px-2" />
+                <input type="text" className="w-full border border-[#8B5CF6] rounded p-1 px-2 " />
               </div>
               <div className="w-[50%] flex flex-col justify-center items-start">
                 <label htmlFor="">City</label>
-                <input type="text" className="w-full border rounded p-1 px-2" />
+                <input type="text" className="w-full border border-[#8B5CF6] rounded p-1 px-2 " />
               </div>
               <div className="w-[50%] flex flex-col justify-center items-start">
                 <label htmlFor="">Phone</label>
-                <input type="text" className="w-full border rounded p-1 px-2"/>
+                <input type="text" className="w-full border border-[#8B5CF6] rounded p-1 px-2 "/>
               </div>
             </div>
           </section>
